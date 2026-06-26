dc0109b1241d35732e86586650a421e7b7822475
SkD <ddavi.soaressk@gmail.com>
2026-06-25 22:38:53 -0300

    feat:menu.html and abouUs.html

--- js/load.js
+++ js/load.js
@@ -1,2 +1,14 @@
-const btn = document.querySelector('.callToAction');
-btn.classList.add('loaded');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target); // anima só 1 vez
+    }
+  });
+}, {
+  threshold: 0.2 // 20% visível já ativa
+});
+
+document.querySelectorAll(".item").forEach(el => {
+  observer.observe(el);
+});
