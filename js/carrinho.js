cac8c507eb5e8852a8a190bb2638f15ff05daf55
SkD <ddavi.soaressk@gmail.com>
2026-09-11 11:07:24 -0300

    feat: order details

--- js/carrinho.js
+++ js/carrinho.js
@@ -138,37 +138,83 @@ mostrarCarrinho();
 // Finalizar pedido (WhatsApp)
 // ===============================
 
+
 function finalizarPedido() {
 
+    const nome = document.getElementById("nome").value.trim();
+    const observacoes = document.getElementById("obs").value.trim();
+    const pagamento = document.getElementById("pag").value;
+
+    // Verifica se o carrinho está vazio
     if (carrinho.length === 0) {
         alert("Seu carrinho está vazio!");
         return;
     }
 
-    let mensagem = " *Pedido do Carrinho:*\n\n";
+    // Verifica se o nome foi preenchido
+    if (nome === "") {
+        alert("Digite seu nome!");
+        document.getElementById("nome").focus();
+        return;
+    }
+
+    // Verifica se a forma de pagamento foi escolhida
+    if (pagamento === "") {
+        alert("Selecione uma forma de pagamento!");
+        document.getElementById("pag").focus();
+        return;
+    }
+
+    // Transforma o valor do select em um texto mais bonito
+    let pagamentoTexto;
+
+    if (pagamento === "debito") {
+        pagamentoTexto = "Débito";
+    } else if (pagamento === "credito") {
+        pagamentoTexto = "Crédito";
+    } else if (pagamento === "pix") {
+        pagamentoTexto = "Pix";
+    }
+
+    let mensagem = "Pedido do Carrinho:\n\n";
+
+    mensagem += `👤 Nome: ${nome}\n\n`;
+
     let total = 0;
 
     carrinho.forEach(item => {
+
         const subtotal = item.preco * item.quantidade;
+
         total += subtotal;
 
         mensagem += `• ${item.nome} x${item.quantidade} = R$ ${subtotal.toFixed(2)}\n`;
     });
 
-    mensagem += `\n💰 *Total: R$ ${total.toFixed(2)}*`;
+    mensagem += `\n📝 Observações: ${observacoes || "Nenhuma"}`;
+
+    mensagem += `\n💳 Pagamento:${pagamentoTexto}`;
+
+    mensagem += `\n\n💰 Total: R$ ${total.toFixed(2)}*`;
 
     navigator.clipboard.writeText(mensagem)
+
         .then(() => {
 
             alert("Pedido copiado! Abrindo WhatsApp...");
 
             const numero = "5511999999999"; // <-- troca aqui
+
             const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
 
             window.open(url, "_blank");
 
         })
+
         .catch(() => {
+
             alert("Não consegui copiar automaticamente 😕");
+
         });
 }
+
